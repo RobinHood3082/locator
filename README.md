2d77e784a4f92ec390b7e22a320ac65286a37012
RobinHood3082 <rkhan3082@gmail.com>
2024-10-20 16:51:14 +0600

    add: initial code and readme

---
+++ README.md
@@ -0,0 +1,48 @@
+# Service Locator
+
+The Service Locator pattern is implemented in this Go package to manage service registration and retrieval. This package allows you to register services as singletons, lazy singletons, or factories, and retrieve them as needed.
+
+## Installation
+
+To install the package, run:
+
+```sh
+go get github.com/RobinHood3082/locator
+```
+
+## Usage
+### Creating a Service Locator
+To create a new instance of the `ServiceLocator`, use the `New` function:
+```go
+import "github.com/RobinHood3082/locator"
+
+sl := locator.New()
+```
+### Registering Services
+#### Registering a Singleton
+To register an already created instance as a singleton:
+```go
+locator.RegisterSingleton(sl, myInstance)
+```
+#### Registering a Lazy Singleton
+To register a provider function that will create a singleton instance on first access:
+```go
+locator.RegisterLazySingleton(sl, func() *MyService {
+    return &NewMyService()
+})
+```
+#### Registering a Factory
+To register a provider function that will create a new instance each time `Get` is called:
+```go
+locator.RegisterFactory(sl, func() *MyService {
+    return &NewMyService()
+})
+```
+#### Retrieving Services
+To retrieve an instance of the requested type:
+```go
+instance, err := servicelocator.GetMyService
+if err != nil {
+    // handle error
+}
+```
---
+++ locator.go
@@ -0,0 +1,100 @@
+package servicelocator
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Provider is a function type that creates instances of services
+type Provider[T any] func() T
+
+// ServiceLocator manages service registration and retrieval
+type ServiceLocator struct {
+	mu        sync.RWMutex
+	instances map[any]any
+	providers map[any]any
+}
+
+// New creates a new ServiceLocator instance
+func New() *ServiceLocator {
+	return &ServiceLocator{
+		instances: make(map[any]any),
+		providers: make(map[any]any),
+	}
+}
+
+// RegisterSingleton registers an already created instance as a singleton
+func RegisterSingleton[T any](sl *ServiceLocator, instance T) {
+	sl.mu.Lock()
+	defer sl.mu.Unlock()
+	sl.instances[getTypeKey[T]()] = instance
+}
+
+// RegisterLazySingleton registers a provider function that will be used to create
+// a singleton instance on first access
+func RegisterLazySingleton[T any](sl *ServiceLocator, provider Provider[T]) {
+	sl.mu.Lock()
+	defer sl.mu.Unlock()
+	sl.providers[getTypeKey[T]()] = &lazySingleton[T]{
+		provider: provider,
+	}
+}
+
+// RegisterFactory registers a provider function that will create a new instance
+// each time Get is called
+func RegisterFactory[T any](sl *ServiceLocator, provider Provider[T]) {
+	sl.mu.Lock()
+	defer sl.mu.Unlock()
+	sl.providers[getTypeKey[T]()] = provider
+}
+
+// Get retrieves an instance of the requested type
+func Get[T any](sl *ServiceLocator) (T, error) {
+	sl.mu.RLock()
+	typeKey := getTypeKey[T]()
+
+	if instance, exists := sl.instances[typeKey]; exists {
+		sl.mu.RUnlock()
+		return instance.(T), nil
+	}
+
+	if provider, exists := sl.providers[typeKey]; exists {
+		sl.mu.RUnlock()
+
+		switch p := provider.(type) {
+		case *lazySingleton[T]:
+			return p.getInstance(sl)
+		case Provider[T]:
+			return p(), nil
+		}
+	} else {
+		sl.mu.RUnlock()
+	}
+
+	var zero T
+	return zero, fmt.Errorf("no provider registered for type %T", zero)
+}
+
+// lazySingleton wraps a provider function and ensures only one instance is created
+type lazySingleton[T any] struct {
+	once     sync.Once
+	instance T
+	provider Provider[T]
+}
+
+// getInstance returns the singleton instance, creating it if necessary
+func (ls *lazySingleton[T]) getInstance(sl *ServiceLocator) (T, error) {
+	ls.once.Do(func() {
+		ls.instance = ls.provider()
+		sl.mu.Lock()
+		sl.instances[getTypeKey[T]()] = ls.instance
+		sl.mu.Unlock()
+	})
+	return ls.instance, nil
+}
+
+// getTypeKey returns a unique key for type T
+func getTypeKey[T any]() any {
+	var zero T
+	return fmt.Sprintf("%T", zero)
+}
